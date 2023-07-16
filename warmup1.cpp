5efebb30cdcb306f3d96829e398845dfc15e0d98
M.Farhan Nur Rohman <52419427+FarhanNurrohman@users.noreply.github.com>
2023-07-16 14:06:31 +0700

    Add files via upload

--- warmup1.cpp
+++ warmup1.cpp
@@ -1,19 +1,40 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-	int p1,p2,b1,b2;
-	cin >> p1 >> p2;
-	cin >> b1 >> b2;
-	int r1 = p1*b2;
-	int r2 = p2*b1;
-	
-	if(r1 > r2){
-		cout << "lebih besar";
-	}else if(r1 < r2){
-		cout << "lebih kecil";
-	}else{
-		cout << "sama";
+
+int checkZero(int a){
+	if(a == 0){
+		return 1;
 	}
-	return 0;
+	return a;
+}
+
+string compareFractions(int a, int b, int c, int d) {
+    int numerator1 = a * d;
+    int numerator2 = c * b;
+    
+    if (numerator1 == numerator2)
+        return "sama";
+    else if (numerator1 > numerator2)
+        return "lebih besar";
+    else
+        return "lebih kecil";
+}
+
+int main()
+{
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+	c = checkZero(c);
+	d = checkZero(d);
+	
+    string result = compareFractions(a, b, c, d);
+    cout << result;
+
+    return 0;
 }
